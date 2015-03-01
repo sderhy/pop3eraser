52e1f4cae54c87753887f62e6e4ca55013f26884
sderhy <sergederhy@MacBookProSerge-4.local>
2015-03-01 12:23:08 +0100

    First working version
    Command line perl utility to delete spam message from your pop3 mail.
    This is the first working version.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # pop3eraser
-I need a simple utility to delete my mails from pop3account.
+I needed a simple command line programm to delete my mails from my pop3account.
+It's a very simple perl command line utility
+I wanted it to work as a vi utility, but I didn't found it so I had to program it.
+
+Requirements: 
+perl installed,with these modules: 
+	-  Term::Screen;
+	-  Switch; 
+	-  Mail::POP3Client;
+	-  MIME::Base64 ;
+Configuration:
+	Please edit the pop3eraser.pl file at the beginning.
+
+Launching: 
+	perl pop3eraser.pl
+	type j or k to walk through your mails subjects
+	type x to mark for deletion.
+	when you are finished, type qq and confirm with y to delete.
+	You can also exit without deleting by typing q our n.
+
+This utility is entirely free.
+If you use it, please send me an email, I would be soooo happy !
+
+Serge Derhy
--- pop3eraser.pl
+++ pop3eraser.pl
@@ -1,16 +1,158 @@
+# This program is aimed at deleting pop3 mail, without looking further than the subject.
+# I am developping it on github as a test please allow some  mistakes !
+# Edit here your configuration:
+	$host =  "maildomain.maildomain.maildomain" ; 
+	$user = "$user" ;
+	$password = "mysecretpassword" ; 
+	#require ("config.pl") ;  # or require a personnal config
+
+# Libraries: 
+# sudo cpan install .. 
+	use Term::Screen;
+	use Switch; 
 	use Mail::POP3Client;
 	use MIME::Base64 ;
-	$pop = new Mail::POP3Client( USER  => "me@github.com", PASSWORD => "wontgiveyou",HOST => "mail.host.com" , AUTH_MODE=>'PASS' );
-	$count = $pop->Count() ; print "$count messages \n" ;
-	#### Chaque message est examiné : 
-	for ($i = 1; $i <= $pop->Count(); $i++) {
-		# Recherche du header s'il s'agit d'un fichier exapaq, is_exapaq=1 ;
-  		foreach ( $pop->Head( $i ) ) { # Analyse de chaque ligne en général une 20 aine de lignes...
-			#if(/^(From|Subject):(.*)/i){ print $_ ."\n" ; 	}
-			#/^From:(.*)/i && printf( "%03d %40.40s |",$i, $1 );
-			/^Subject:(.*)/i && printf( "%03d >> %-60s\n", $i,  $1 );
-		}
-	}#fin de for
+	use Encode qw/encode decode/;
+
+
+# Global variables: 
+	($x,$y) = (0,0) ; # global variables to maintain the screen 
+	@froms=() ;
+	$scr ; 
+	@todelete = () ; 
+
+# Connexion, and get the froms and subjects: 
+	$pop = connexion() ; 
+	$count = $pop->Count() ;# number of messages
+	@subjects = getSubjects($pop) ; 
+
+# Use the terminal :
+	 usescreen( @subjects) ; 
 	$pop->Close();
 
+	exit(0) ; 
+
+#### Below are functions only ###
+sub connexion(){
+	$pop = new Mail::POP3Client( 
+			USER  =>$user, 
+			PASSWORD => $password ,
+			HOST => $host ,
+			AUTH_MODE=>'PASS' );
+	return $pop ; 
+}
+
+sub getSubjects($pop){
+	my $pop = shift ; 
+	my $from = ''; 
+	my @subjects = ()  ; 
+	for ($i = 1; $i <= $pop->Count(); $i++) {
+
+  		foreach ( $pop->Head( $i ) ) { 
+			if(/^From: ?(.*)/i){
+				$from   =   decode('MIME-HEADER', $1)  ;
+				$from   =~s/"//g ;
+				$froms[$i] = $from ; 
+				$from ='';
+			}
+			if(/^Subject:(.*)/i){
+				$subjects[$i] =  decode('MIME-HEADER', $1)  ; 
+			}
+			
+			
+		}#end foreach
+	}#End of for 
+	return   @subjects ; 
+}
+
+sub usescreen(){
+
+	my @subjects = @_ ; 
+	my $count = scalar(@subjects) ;
+	$scr = new Term::Screen;
+	unless ($scr) { die " Something's wrong \n"; }
+	$scr->clrscr();
+	my $rows = $scr -> rows() ; 
+	my $cols = $scr -> cols() ; 
+
+	my $numLines = $count > $rows ? $rows : $count ; 
+	my $i  ; 
+	
+	status($scr, "There is ". ($count -1)  . " messages . " )  ; 
+	my $line= '';
+	foreach my $x(1 .. $cols){ $line .= '-' ; }
+	$scr->at(5,0) ->puts(  $line ) ;
+	$scr->at(6,0) ->puts( "Strike  j to go down , k up , x mark for deletion , qq to quit and y to confirm")  ;  
+
+	my $c =''; 
+	$scr->noecho() ; 
+	while($c ne 'q' ){
+		$c = $scr->getch();      # doesn't need Enter key 
+		charnavigation($scr, $c) ; 
+	}
+	$c= $scr->getch() ;
+	status( $scr , "Delete ".countMsg()." mails ? n or y  ") ; 
+	while(1){
+		$c= $scr->getch() ;
+		if($c eq 'y') {
+			erase() ; 
+			last ; 
+		}
+		if($c =~m/[nqNQ]/) { last ; } 
+	}
+}
+
+sub erase{
+	my $i = 0 ; 
+			
+	$scr->clreol() -> at(0,0) -> puts("Deleted messages: ") ; 	
+	for($i=0; $i < scalar(@todelete) ; $i++ ) {
+		if($todelete[$i] eq "X"){
+			$pop->Delete($i);
+			$scr->bold()->puts("...$i ") ->normal() ; 
+		}
+	}
+}
+
+# See also Term::ReadKey
+sub charnavigation(){
+	my ($scr , $c ) =  @_ ; 
+	#status($scr, "i: $i , count: $count" ) ; 
+	switch($c){
+		case('k') { 
+			$i=$i-1 ; 
+			if($i==0){  $i=1 ; }
+		} 
+		case('j') { 
+			$i++ ; 
+			if($i > $count) {  $i=$count  ;  }
+		} 
+		case('x'){
+			if($todelete[$i] eq "X") {  $todelete[$i] =''} else {  $todelete[$i]="X" ;}
+		}
+	}
+	displayMessage($scr, $i , 2, 0 ) ; 
+	
+}
+
+sub displayMessage {
+		my($scr, $i ,$r,$c) = @_ ; 
+		$scr ->at($r,$c) ->clreol() ->normal()-> puts( $froms[$i])  ; 	
+		$scr ->at($r+1,$c) ->clreol() ->bold() ->puts($i ) ->normal()-> puts(" [".$todelete[$i]."] ") ->puts( $subjects[$i])  ; 	
+		$scr ->at($r+2,$c) ->clreol() ; 
+		return $scr ;
+}
+
+sub status{
+	my ($scr, $msg)  = @_ ; 
+	$scr->clreol() -> at(0,0) -> puts($msg) ; 	
+}
+
+sub countMsg{
+	my $totalX = O ; 
+	foreach my $x(1 .. $count){
+		if($todelete[$x] eq "X"){  $totalX = $totalX+1 ; }
+	}
+	return $totalX ; 
+}
 exit(0) ;
